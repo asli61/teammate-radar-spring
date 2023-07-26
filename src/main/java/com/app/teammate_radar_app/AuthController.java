6ca203a49816b6b6d923031b4e3e51a6805eff2d
asim61 <asimersoy2005@gmail.com>
2023-07-26 14:56:10 +0100

    User and Role entities and repositories created.

--- src/main/java/com/app/teammate_radar_app/AuthController.java
+++ src/main/java/com/app/teammate_radar_app/AuthController.java
@@ -1,21 +1,27 @@
 package com.app.teammate_radar_app;
 
-
+import com.app.teammate_radar_app.Repos.UserRepository;
+import jakarta.el.BeanNameResolver;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
     private final UserRepository userRepository;
+    BeanNameResolver context;
 
     public AuthController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    @GetMapping("/test")
-    public Iterable register()
+
+    @GetMapping("/register")
+    public void register(/*@RequestBody*/)
     {
-        return userRepository.findAll();
+
+
+        //return userRepository.save(product);
     }
 }
---
+++ src/main/java/com/app/teammate_radar_app/Repos/User.java
@@ -0,0 +1,32 @@
+package com.app.teammate_radar_app.Repos;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    @JoinTable(
+            name="users_roles",
+            joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="ID")},
+            inverseJoinColumns={@JoinColumn(name="ROLE_ID", referencedColumnName="ID")})
+    private List<Role> roles = new ArrayList<>();
+}
