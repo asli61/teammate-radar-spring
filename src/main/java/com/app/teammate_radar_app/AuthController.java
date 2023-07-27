e6c515e2667dcacdd173d582b5512bbd80265af5
asim61 <asimersoy2005@gmail.com>
2023-07-27 13:18:53 +0100

    register and login system created. Security config updated

--- src/main/java/com/app/teammate_radar_app/AuthController.java
+++ src/main/java/com/app/teammate_radar_app/AuthController.java
@@ -1,27 +1,63 @@
 package com.app.teammate_radar_app;
 
+import com.app.teammate_radar_app.Repos.User;
 import com.app.teammate_radar_app.Repos.UserRepository;
 import jakarta.el.BeanNameResolver;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
     private final UserRepository userRepository;
-    BeanNameResolver context;
 
     public AuthController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    @Getter
+    @Setter
+    @RequiredArgsConstructor
+    public class RegisterApiDto {
+        @NotNull
+        private String email;
+        @NotNull @NotBlank
+        private String password;
+
+        private int role;
+    }
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
 
     @GetMapping("/register")
-    public void register(/*@RequestBody*/)
+    public ResponseEntity<?> register(@Valid RegisterApiDto requestObject, BindingResult bindingResult)
     {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
 
+        String email = requestObject.getEmail();
+        String password = requestObject.getPassword();
 
-        //return userRepository.save(product);
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole(null);
+
+        System.out.println(user);
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/com/app/teammate_radar_app/Repos/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.teammate_radar_app.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
