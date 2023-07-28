d51fb7cf8528540e25bad1660dbdc22b79ebc68d
AliErsoy <aliersoytr35@gmail.com>
2023-07-28 12:41:16 +0100

    files put into their correct folder

--- src/main/java/com/app/teammate_radar_app/ResponseEntityBuilder.java
+++
@@ -1,9 +0,0 @@
-package com.app.teammate_radar_app;
-
-import  org.springframework.http.ResponseEntity;
-
-public class ResponseEntityBuilder {
-    public static ResponseEntity<Object> build(LoginRegisterErrorHandler loginregistererrorhandler) {
-        return new ResponseEntity<>(loginregistererrorhandler, loginregistererrorhandler.getStatus());
-    }
-}
